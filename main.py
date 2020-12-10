a24c03e3ddb98e98fd37a331af9b13d7e1e0c06f
Icaro Onofre Silva <xdollboyx@gmail.com>
2020-12-10 17:17:34 -0300

    Commit

--- main.py
+++ main.py
@@ -1,6 +1,111 @@
-import pdb
-import random
-def printarnumeros():
-    print(random.randint(3,9))
+import os
+import requests
+from bs4 import BeautifulSoup
 
-printarnumeros()
+#search_length = int(input("digite o tamanho da pesquisa "))
+
+#a = "terrenos na regiao de santos"
+source = "https://www.zapimoveis.com.br/venda/imoveis/sp+santos"
+# donwloading source pages
+r = requests.get(source[0:len(source)])
+htmlsource = BeautifulSoup(r.text, 'lxml')
+print(htmlsource.prettify())
+match = htmlsource.html
+print(match)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
