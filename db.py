cd0c4dc2402f4a45675dc7bcd19615ebc20eabe1
Icaro Onofre Silva <xdollboyx@gmail.com>
2021-01-05 12:15:22 -0300

    geocode modification

--- db.py
+++ db.py
@@ -1,34 +1,77 @@
-import main
+import scrapper
 import sqlite3
 #create db
-def query():
-    database = sqlite3.connect('moradia_custo.db') 
-    db = database.cursor()
-    #db.execute('drop table moradia')
+database = sqlite3.connect('moradia_custo.db') 
+db = database.cursor()
 
-    """
-    db.execute('''CREATE TABLE IF NOT EXISTS moradia(
-            id moradia PRIMARY KEY AUTO_INCREMENT,
-            preco text NOT NULL,
-            moradia text
-    );''')
-    """
+def insert_into_table(preco,endereco,latitude,longitue):
+    # Create query
+    succ = True
+    try:
+        for i in range(len(preco)):
+            db.execute('''INSERT INTO moradia (preco,endereco,latitude,longitude) VALUES ('{}','{}','{}','{}')'''.format(preco[i],endereco[i],latitude[i],longitude[i]))
+    except:
+        succ = False 
+    print(succ)
 
-    #for a in db.execute('SELECT * FROM moradia'):
-        #print(a)
+    database.commit()
 
-    result = list(main.Scrapper())
-    endereco = []
-    preco= []
-    for i in range(19):
-        preco.append(result[0][i])
-        print(preco[i])
-    for i in range(19):
-        endereco.append(result[1][i])
-        print(endereco[i])
+    database.close()
 
-    #for a in range(len(result)):
-        #print(result[a])
-        #db.execute('INSERT INTO moradia (preco,moradia) VALUES (%s,%s)', %(preco,moradia)
+def recreate_table():
 
-query()
+    try:
+        db.execute('''CREATE TABLE moradia(
+                 id moradia PRIMARY KEY ,
+                 preco text NOT NULL,
+                 endereco text,
+                 latitude text,
+                 longitude text
+         );''')
+    except:
+
+        db.execute('''DROP TABLE moradia''')
+
+        db.execute('''CREATE TABLE moradia(
+                 id moradia PRIMARY KEY ,
+                 preco text NOT NULL,
+                 endereco text,
+                 latitude text,
+                 longitude text
+         );''')
+
+        print("table created")
+
+       
+    database.commit()
+
+    database.close()
+
+def select_table():
+    db.execute("SELECT * FROM moradia")
+    results = db.fetchall()
+
+    for i in results:
+        print(i)
+
+# Uncomment for testing and running //////////////////////////////////////////
+
+# recreate_table()
+select_table()
+
+preco = []
+endereco = []
+latitude = []
+longitude = []
+data = scrapper.Scrapper()
+
+for i in range(20): 
+    try:
+        preco.append(data[0][i])
+        endereco.append(data[1][i])
+        latitude.append(data[2][i].latitude)
+        longitude.append(data[2][i].longitude)
+    except:
+        pass
+
+# insert_into_table(preco,endereco,latitude,longitude)
---
+++ scrapper.py
@@ -0,0 +1,37 @@
+import pdb
+from geopy.geocoders import  Nominatim
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+
+import requests
+
+#Scrapper get the relevant data from the souce
+def Scrapper():
+    options = Options()
+    options.headless = True 
+
+#url source 
+    source = "https://www.zapimoveis.com.br/venda/imoveis/sp+santos"
+
+#Initialize web driver 
+    driver = webdriver.Firefox(options=options)
+    driver.get(source)
+
+#Initialize empty list
+    preco = []
+    endereco = []
+
+#Gets data from the site´s source
+    for a in range(1,25):
+        preco_source = driver.find_element_by_xpath("/html/body/main/section/div[2]/div[2]/section/div/div[{}]/div/div[2]/div[1]/p/strong".format(str(a)))
+        endereco_source = driver.find_element_by_xpath("/html/body/main/section/div[2]/div[2]/section/div/div[{}]/div/div[2]/div[3]/p".format(str(a)))
+        preco.append(preco_source.text)
+        endereco.append(endereco_source.text)
+# def geocode_generator():
+    geocode = []
+    geolocator = Nominatim(user_agent="Preco_moradia")
+    for i in range(len(endereco)):
+        geocode.append(geolocator.geocode(endereco[i]))
+    driver.close()
+    return preco,endereco,geocode
